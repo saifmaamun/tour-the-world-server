416fe9aaa5dcb5aa3db5396858cf5d446cbe6ad9
unknown <sadifmamun@gmail.com>
2021-10-30 14:39:02 +0600

    2nd

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 
 // 
@@ -22,6 +23,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         console.log('from function')
+        await client.connect();
+        const database = client.db("tourTheWorld");
+        const tourCollaction = database.collection("tours");
+
+        // GET API
+        app.get('/destinations', async (req, res) => {
+            const cursor = tourCollaction.find({});
+            const destinations = await cursor.toArray();
+            res.send(destinations);
+        })
+
+        // GET A SINGEL API
+        app.get('/destinationDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('specific id',id)
+            const query = { _id: ObjectId(id) };
+            const destination = await tourCollaction.findOne(query);
+            res.json(destination)
+        })
+
+        // POST API
+        app.post('/destinations', async (req, res) => {
+            const destination = req.body;
+            console.log('hitting', destination)
+
+            const result = await tourCollaction.insertOne(destination);
+            console.log(result)
+            res.json(result)
+        })
+
+
     } finally {
         // await client.close();
     }
